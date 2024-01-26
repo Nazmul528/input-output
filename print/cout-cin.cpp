d1e02b93fa9e8d0c1e9fd92000430445599e34ff
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2024-01-26 19:18:58 +0600

    Create cout-cin.cpp

---
+++ print/cout-cin.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter the integer number :";
+    cin>>num;
+    cout<<"the number :"<<num;
+    getch();
+}
